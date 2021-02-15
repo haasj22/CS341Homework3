87a080957cc791249c38ee794843cace5fbc52a4
johnhaas <haasj22@up.edu>
2021-02-15 12:10:18 -0800

    final_push

---
+++ hw3/tests/sum.test.js
@@ -0,0 +1,4 @@
+const sum = require('/Users/johnhaas/Dropbox/My\ Mac\ \(John’s\ MacBook\ Pro\)/Downloads/CS341/HW3/CS341Homework3/hw3/public/javascripts/sum.js');
+     test('adds 1 + 2 to equal 3', () => {
+	expect(sum(1,2)).toBe(3);
+}) 
